617f7938fe0dd4c1d23dda8611c496e4098cdb1c
rlc900 <rianna.cleary@gmail.com>
2020-07-31 15:53:42 -0400

    installed socket.io-client

---
+++ src/Components/Chat/Chat.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+// this module will help retrieve data from URL
+import queryString from 'query-string';
+import io from 'socket.io-app';
+
+import './Chat.css'
+
+function Chat() {
+  return (
+    <h1>Chat</h1>
+  )
+}
+
+export default Chat;
--- src/Components/Join/Join.js
+++ src/Components/Join/Join.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom'
 
+import './Join.css'
 
 function Join() {
   const [name, setName] = useState('');
@@ -13,7 +14,7 @@ function Join() {
         <h1 className='heading'>Join</h1>
         <div><input placeholder='Name' className='joinInput' type='text' onChange={(evt) => setName(evt.target.value)}/></div>
         <div><input placeholder='Room' className='joinInput mt-20' type='text' onChange={(evt) => setRoom(evt.target.value)}/></div>
-          <Link>
+          <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&=${room}`}>
           <button className='button mt-20' type='submit'>Sign In</button>
           </Link>
       </div>
