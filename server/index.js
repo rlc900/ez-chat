e75788c4f1cd682ab3d85741c7cf28be0fed1ac6
rlc900 <rianna.cleary@gmail.com>
2020-08-11 13:07:28 -0400

    updated index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require("socket.io");
 const http = require('http');
 
+const {addUser, removeUser, getUser, getUsersInRoom } = require('./users.js')
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -13,12 +15,24 @@ const io = socketio(server);
 
 // socket that is connected to client side socket
 io.on('connection', (socket) => {
-  console.log('we have a new connection!!!!!!!!!')
-
   socket.on('join', ({ name, room }, callback) => {
-    console.log(name, room)
+    const {error, user} = addUser({ id: socket.id, name, room });
 
-  })
+    if (error) return callback(error);
+
+    // this welcomes users to chat (admin generated message); happening on frontend
+    socket.emit('message', {user: 'admin', text: `${user.name}, welcome to the room ${user.room}`})
+    // socket.broadcast sends a message to everyone BUT the user
+    socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name} had joined!`})
+    socket.join(user.room);
+    callback();
+  });
+
+  // user generated message; we wil be waiting (expecting) on sendMessage
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+  });
 
   socket.on('disconnect', () => {
     console.log('user just left!!!!!!!')
--- server/users.js
+++ server/users.js
@@ -22,7 +22,7 @@ const addUser = ({id, name, room}) => {
 const removeUser = (id) => {
   const index = users.findIndex((user) => user.id === id)
 
-  if (index !=== -1) {
+  if (index !== -1) {
     return users.splice(index, 1)[0];
   }
 };
@@ -30,3 +30,5 @@ const removeUser = (id) => {
 const getUser = (id) => users.find(user => user.id === id);
 
 const getUsersInRoom = (room) => users.filter((user) => user.room === room);
+
+module.exports = { addUser, removeUser, getUser, getUsersInRoom }
