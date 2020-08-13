3ba7aeb9fd55d4efb3c59c8a78ca77a6bc0d9e25
rlc900 <rianna.cleary@gmail.com>
2020-08-13 14:57:03 -0400

    added fonts

--- src/Components/Join/Join.js
+++ src/Components/Join/Join.js
@@ -11,7 +11,7 @@ function Join() {
   return (
     <div className='joinOuterContainer'>
       <div className='joinInnerContainer'>
-        <h1 className='heading'>Join</h1>
+        <h1 className='heading'>EZ-Chat</h1>
         <div><input placeholder='Name' className='joinInput' type='text' onChange={(evt) => setName(evt.target.value)}/></div>
         <div><input placeholder='Room' className='joinInput mt-20' type='text' onChange={(evt) => setRoom(evt.target.value)}/></div>
           <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
