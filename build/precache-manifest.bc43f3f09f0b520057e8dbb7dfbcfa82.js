a20527e1ef2aaf20786413c80e05ba328b09e0ab
Rianna <rianna.cleary@gmail.com>
2020-08-14 17:52:12 -0400

    organized

---
+++ build/precache-manifest.bc43f3f09f0b520057e8dbb7dfbcfa82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "711f99433a5ce1271a975e897d6cf543",
+    "url": "/chat-app/index.html"
+  },
+  {
+    "revision": "e35d73ca8ebe653b5532",
+    "url": "/chat-app/static/css/main.20b92013.chunk.css"
+  },
+  {
+    "revision": "6e243c2234111cec8b57",
+    "url": "/chat-app/static/js/2.f85f3420.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/chat-app/static/js/2.f85f3420.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e35d73ca8ebe653b5532",
+    "url": "/chat-app/static/js/main.cee79542.chunk.js"
+  },
+  {
+    "revision": "a46df673de3fe3887809",
+    "url": "/chat-app/static/js/runtime-main.0c4eda3f.js"
+  }
+]);
